3c41dc59b62d8d27b4a81e172c6a4ba2bd1699da
nanjizal <nanjizal@users.noreply.github.com>
2019-12-24 09:18:15 +0000

    Update ScaleTranslateContext.hx

--- justPath/transform/ScaleTranslateContext.hx
+++ justPath/transform/ScaleTranslateContext.hx
@@ -1,5 +1,5 @@
-package trilateral.justPath.transform;
-import trilateral.justPath.IPathContext;
+package justPath.transform;
+import justPath.IPathContext;
 class ScaleTranslateContext implements IPathContext {
     public var pathContext: IPathContext;
     public var dx: Float;
