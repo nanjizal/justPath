e578e0856f18c53eb1f41d3290d739cf6b7e6a4f
nanjizal <none>
2020-07-09 16:19:21 +0100

    add dox

---
+++ justPath/upkeep/Doc.hx
@@ -0,0 +1,22 @@
+package justPath.upkeep;
+import justPath.EllipseArc;
+import justPath.IPathContext;
+import justPath.PathContextTrace;
+import justPath.StoreF6;
+import justPath.StoreF8;
+import justPath.SvgPath;
+import justPath.transform.M3;
+import justPath.transform.ScaleContext;
+import justPath.transform.ScaleTranslateContext;
+import justPath.transform.TransformationContext;
+import justPath.transform.TranslationContext;
+
+@:dox(hide)
+class Doc {
+    public static function main(){
+        
+    }
+    public function new(){
+        
+    }
+}
