d4ac4fffda92bdda4ed5d5d2a7076a6456433c70
nanjizal <nanjizal@users.noreply.github.com>
2022-12-12 00:49:13 +0000

    Update ILinePathContext.hx

--- justPath/ILinePathContext.hx
+++ justPath/ILinePathContext.hx
@@ -3,4 +3,6 @@ package justPath;
 interface ILinePathContext{
     public function moveTo( x: Float, y: Float ): Void;
     public function lineTo( x: Float, y: Float ): Void;
+    // to allow the lineTo from curves to be used differently.
+    public function lineSegmentTo( x: Float, y: Float ): Void;
 }
